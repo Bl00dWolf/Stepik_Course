3fa2300b2d7574147a27172cc004d1df52417150
Bl00dWolf <steamserver2@gmail.com>
2022-10-11 11:31:28 +0300

    Программа - генератор паролей по параметрам. В стадии разработки.

---
+++ Password_Gen.py
@@ -0,0 +1,25 @@
+import random
+
+digits = '0123456789'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+punctuation = '!#$%&*+-=?@^_'
+chars = ''
+
+pass_num = int(input('Введите целое число - количество паролей для генерации: '))
+pass_len = int(input('Введите целое число - длинну паролей: '))
+
+pass_ABC = input('Должны ли быть в паролях прописные (большие) буквы? Д - да Н - нет\n')
+pass_abc = input('Должны ли быть в паролях строчные (маленькие) буквы? Д - да Н - нет\n')
+pass_specials = input('Должны ли быть в паролях специальные (!#$%&*+-=?@^_) символы? Д - да Н - нет\n')
+pass_similar = input('Исключать ли неоднозначные символы (il1Lo0O) в паролях? Д - да Н - нет\n')
+
+if pass_ABC.lower() == 'д':
+    chars += uppercase_letters
+if pass_abc.lower() == 'д':
+    chars += lowercase_letters
+if pass_specials.lower() == 'д':
+    chars += pass_specials
+if pass_similar.lower() == 'д':
+    for c in 'il1Lo0O':
+        chars.replace(c, '')
