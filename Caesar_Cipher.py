f97f9c6dbde5a3801b23d21c2d3af0e52c997f7f
Bl00dWolf <steamserver2@gmail.com>
2022-10-12 21:39:54 +0300

    Шифр Цезаря. Разработка

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,6 +1,6 @@
-choice = input('Шифр Цезаря\nЧто вы хотите сделать с текстом? Ш - шифровать Д - расшифровать')
-lang = input('Какой язык текста будет использован? Р - русский А - английский')
-delta = int(input('Задайте шаг сдвига (целое число)'))
+choice = input('Шифр Цезаря\nЧто вы хотите сделать с текстом? Ш - шифровать Д - расшифровать\n')
+lang = input('Какой язык текста будет использован? Р - русский А - английский\n')
+delta = int(input('Задайте шаг сдвига (целое число)\n'))
 
 chars_ru = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
 chars_eng = 'abcdefghijklmnopqrstuvwxyz'
@@ -8,7 +8,9 @@ chars_eng = 'abcdefghijklmnopqrstuvwxyz'
 
 def crypt(string, delta):
     for c in string:
-        string = string.replace(c, chr(ord(c) + delta))
+        if c in '1234567890,./: !?':
+            continue
+        string = string.lower().replace(c, chars_ru[(chars_ru.find(c) + delta) % 32])
     return string
 
 
