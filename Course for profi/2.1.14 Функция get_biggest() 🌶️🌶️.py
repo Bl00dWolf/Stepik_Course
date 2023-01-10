a60eefb66a4a89f60168477dfd3f46b252437277
Bl00dWolf <steamserver2@gmail.com>
2023-01-10 23:12:56 +0300

    Прохождение курса для профи. 4.2 Решение задач и 2.2 Закрываем долги

--- Course for profi/2.1.14 Функция get_biggest() 🌶️🌶️.py
+++ Course for profi/2.1.14 Функция get_biggest() 🌶️🌶️.py
@@ -1,7 +1,7 @@
 def get_biggest(numbers: list[int]) -> int:
     if numbers:
         max_len_num = max(numbers, key=lambda x: len(str(x)))
-        num_str = sorted(map(lambda x: str(x), numbers), key=lambda x: x * max_len_num, reverse=True)
+        num_str = sorted(map(lambda x: str(x), numbers), key=lambda x: x * len(str(max_len_num)), reverse=True)
         return int(''.join(num_str))
     else:
         return -1
