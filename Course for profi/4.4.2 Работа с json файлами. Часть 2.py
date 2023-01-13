c1053c05ac1ad4138fb5e3d76baa0910bfe8b00d
Bl00dWolf <steamserver2@gmail.com>
2023-01-13 14:30:55 +0300

    Прохождение курса для профи. 4.4 работа с JSON

---
+++ Course for profi/4.4.2 Работа с json файлами. Часть 2.py
@@ -0,0 +1,17 @@
+import json
+
+words = {
+         frozenset(["tap", "telephone"]): ("tæp", "telifəun"),
+         "travel": "trævl",
+         ("hello", "world"): ("həˈləʊ", "wɜːld"),
+         "moonlight": "muːn.laɪt",
+         "sunshine": "ˈsʌn.ʃaɪn",
+         ("why", "is", "so", "difficult"): ("waɪ", "ɪz", "səʊ", "ˈdɪfɪkəlt"),
+         "adventure": "ədˈventʃər",
+         "beautiful": "ˈbjuːtɪfl",
+         frozenset(["spoon", "block"]): ("spu:n", "blɔk"),
+         "bicycle": "baisikl",
+         ("pilot", "fly"): ("pailət", "flai")
+        }
+
+data_json = json.dumps(words, skipkeys=True)
