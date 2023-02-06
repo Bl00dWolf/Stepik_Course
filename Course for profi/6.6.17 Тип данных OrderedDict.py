3c2fe804e9cdbe7b01b04721b715c4723576ad72
Bl00dWolf <steamserver2@gmail.com>
2023-02-06 22:43:22 +0300

    Прохождение курса для профи. 6.6 работа с OrderedDict

---
+++ Course for profi/6.6.17 Тип данных OrderedDict.py
@@ -0,0 +1,7 @@
+from collections import OrderedDict
+
+data = OrderedDict({'Name': 'Брусника', 'IsNetObject': 'да', 'OperatingCompany': 'Брусника', 'TypeObject': 'кафе',
+                    'AdmArea': 'Центральный административный округ', 'District': 'район Арбат',
+                    'Address': 'город Москва, переулок Сивцев Вражек, дом 6/2', 'SeatsCount': '10'})
+
+print(OrderedDict(reversed(data.items())))
---
+++ Course for profi/6.6.18 Тип данных OrderedDict.py
@@ -0,0 +1,6 @@
+from collections import OrderedDict
+
+data = OrderedDict({'Name': 'Брусника', 'IsNetObject': 'да', 'OperatingCompany': 'Брусника', 'TypeObject': 'кафе',
+                    'AdmArea': 'Центральный административный округ', 'District': 'район Арбат',
+                    'Address': 'город Москва, переулок Сивцев Вражек, дом 6/2', 'SeatsCount': '10'})
+
