7c235684083f575fe391e31a8c6ef687954ba50c
Bl00dWolf <steamserver2@gmail.com>
2023-03-10 12:51:59 +0300

    Прохождение курса для профи. 10.1 Итераторы. Часть 1 - модуль завершен

---
+++ Course for profi/10.1.19 Итераторы. Часть 1.py
@@ -0,0 +1,7 @@
+numbers = [100, 70, 34, 45, 30, 83, 12, 83, -28, 49, -8, -2, 6, 62, 64, -22, -19, 61, 13, 5, 80, -17, 7, 3, 21, 73, 88, -11, 16, -22]
+num_iter = iter(numbers)
+
+next(num_iter)
+next(num_iter)
+next(num_iter)
+print(next(num_iter))
