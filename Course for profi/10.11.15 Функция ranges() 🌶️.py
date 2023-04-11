f6bd916c844143be9857430aa7654c83ad8e11df
Bl00dWolf <steamserver2@gmail.com>
2023-04-11 12:22:13 +0300

    Прохождение курса для профи. 10.11 Модуль itertools. Часть 4 завершена

--- Course for profi/10.11.15 Функция ranges() 🌶️.py
+++ Course for profi/10.11.15 Функция ranges() 🌶️.py
@@ -2,7 +2,12 @@ from itertools import groupby
 
 
 def ranges(numbers: list[int]) -> list[tuple]:
-    gp = groupby(numbers, key=)
+    ranges = []
+    gp = groupby(numbers, key=lambda x: x - numbers.index(x))
+    for k, v in gp:
+        tempo = list(v)
+        ranges.append((min(tempo), max(tempo)))
+    return ranges
 
 
 numbers = [1, 2, 3, 4, 7, 8, 10]
