9983781b2ce4da2170f44cc069a23fee5d57b91b
Bl00dWolf <steamserver2@gmail.com>
2023-09-22 14:05:51 +0300

    Курс ООП Раздел 7.4 Наследование. Часть 4 завершена

--- Course OOP/7. Наследование и полиморфизм/7.4 Наследование. Часть 4/7.4.23 Класс MutableString.py
+++ Course OOP/7. Наследование и полиморфизм/7.4 Наследование. Часть 4/7.4.23 Класс MutableString.py
@@ -1,5 +1,49 @@
 from collections import UserString
 
+
 class MutableString(UserString):
+    def __init__(self, seq):
+        super().__init__(seq)
+        self._str = seq
+
+    def __str__(self):
+        return f'{self._str}'
+
+    def __setitem__(self, key, value):
+        self._str = self._str[:key] + value + self._str[key + 1:]
+
+    def __delitem__(self, key):
+        self._str = self._str[:key] + self._str[key + 1:]
+
     def lower(self):
-        pass
+        self._str = super().lower()
+
+    def upper(self):
+        self._str = super().upper()
+
+    def sort(self, key=None, reverse=False):
+        self._str = ''.join(sorted(str(self._str), key=key, reverse=reverse))
+
+# нормальное решение:
+# from collections import UserString
+#
+#
+# class MutableString(UserString):
+#     def __setitem__(self, index, value):
+#         data_as_list = list(self.data)
+#         data_as_list[index] = value
+#         self.data = "".join(data_as_list)
+#
+#     def __delitem__(self, index):
+#         data_as_list = list(self.data)
+#         del data_as_list[index]
+#         self.data = "".join(data_as_list)
+#
+#     def upper(self):
+#         self.data = self.data.upper()
+#
+#     def lower(self):
+#         self.data = self.data.lower()
+#
+#     def sort(self, key=None, reverse=False):
+#         self.data = "".join(sorted(self.data, key=key, reverse=reverse))
