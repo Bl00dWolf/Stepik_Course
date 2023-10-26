7d28d581cb452b077e0d09117d18a4a6b928eaa3
Bl00dWolf <steamserver2@gmail.com>
2023-10-26 11:57:48 +0300

    Курс ООП Раздел 8.5 Декораторы. Часть 2

--- Course OOP/8. Дополнительные возможности/8.5 Декораторы. Часть 2/8.5.15 Декоратор @auto_repr🌶️.py
+++ Course OOP/8. Дополнительные возможности/8.5 Декораторы. Часть 2/8.5.15 Декоратор @auto_repr🌶️.py
@@ -7,27 +7,25 @@ def auto_repr(args, kwargs):
 
         @functools.wraps(old_repr)
         def new_repr(self):
-            args_values = [getattr(self, x) for x in args]
+            args_values = [repr(getattr(self, x)) for x in args]
             kwargs_dict = {k: v for k, v in zip(kwargs, [getattr(self, x) for x in kwargs])}
-            kwargs_str = [f'{k}={repr(v)}' for (k, v) in kwargs_dict.items()]
-            if kwargs_str:
-                return f'{cls.__name__}({", ".join(map(str, args_values))}, {", ".join(kwargs_str)})'
-            else:
-                return f'{cls.__name__}({", ".join(args_values)})'
+            kwargs_str = [f"{k}={repr(v)}" for (k, v) in kwargs_dict.items()]
+            return f'{cls.__name__}({", ".join(map(str, args_values + kwargs_str))})'
 
         cls.__repr__ = new_repr
         return cls
 
     return wrapper
 
-
-@auto_repr(args=['name', 'surname'], kwargs=[])
-class Person:
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-
-
-person = Person('Gvido', 'van Rossum')
-
-print(person)
+# норм решение:
+# def auto_repr(args, kwargs):
+#     def wrapper(cls):
+#         def __repr__(self):
+#             cls_args = [repr(self.__dict__[k]) for k in args]
+#             cls_kwargs = [f'{k}={self.__dict__[k]!r}' for k in kwargs]
+#             return f'{type(self).__name__}({", ".join(cls_args + cls_kwargs)})'
+#
+#         cls.__repr__ = __repr__
+#         return cls
+#
+#     return wrapper
